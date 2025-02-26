6f005e7c389350888c4da1ed6f69390b22f4fd08
laurenCho9 <lauren.choux@gmail.com>
2025-02-27 04:37:23 +0900

    style: not found 페이지

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,9 @@
 const NotFound = () => {
   return (
-    <>
-      <h1>404 Not Found</h1>
-    </>
+    <div className="notfound">
+      <h3>404 Not Found</h3>
+      <p>페이지를 찾을 수 없습니다</p>
+    </div>
   );
 };
 
